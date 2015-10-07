4bbd80a18936de6994684ea503164cfcef421f2c
aanibaba <ayoolaanibaba@gmail.com>
2015-10-07 14:43:16 -0500

    new

--- app/models/user.js
+++ app/models/user.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 var userSchema = mongoose.Schema({
 	local: {
 		username: String,
-		password:String
-   }
+		password: String
+	}
 });
 
 module.exports = mongoose.model('User', userSchema);
---
+++ config/auth.js
@@ -0,0 +1,8 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '1174599199220707',
+		'clientSecret': '33753f03dff2148befb3d36d4770e882',
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+	}
+
+}
